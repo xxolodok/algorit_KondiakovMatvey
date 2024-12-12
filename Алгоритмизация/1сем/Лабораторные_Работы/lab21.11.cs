f5d02432b959d641e7d6227a36f335027f906526
Xterri-prog <matveik157@gmail.com>
2024-12-12 20:02:02 +0600

    довалены задачи после 24.10 и обновлены прошлые

--- Алгоритмизация/1сем/Лабораторные_Работы/lab21.11.cs
+++ Алгоритмизация/1сем/Лабораторные_Работы/lab21.11.cs
@@ -1,61 +1,51 @@
 using System;
 
+
+
 class lab
 {
     static void Main()
     {
-
         string str = Console.ReadLine();
         int countAnB = 0;
-        int countABC = 0;
-        int maxCountABC = 0;
-        while (true)
+        int currentLength = 0;
+        int currentMax = 0;
+        int maxLength = 0; while (true)
         {
-            countABC = 0;
-
-            if (str == "")
+            if (string.IsNullOrEmpty(str))
             {
                 break;
             }
-            str = str.ToLower();
             for (int i = 0; i < str.Length - 2; i++)
             {
-
                 if ($"{str[i]}" == "a" && $"{str[i + 2]}" == "b")
                 {
                     countAnB++;
-                    break;
                 }
-
             }
+
+
 
             foreach (char c in str)
             {
                 if (char.ToLower(c) == 'a' || char.ToLower(c) == 'b' || char.ToLower(c) == 'c')
                 {
-                    countABC++;
+                    currentLength++;
+                    currentMax = Math.Max(currentMax, currentLength);
                 }
                 else
                 {
-                    if (countABC > maxCountABC)
-                    {
-                        maxCountABC = countABC;
-                    }
-                    countABC = 0; 
+                    currentLength = 0; // Сброс длины, если символ не a, b или c
                 }
-            }
-            if (countABC > maxCountABC)
-            {
-                maxCountABC = countABC;
-                Console.WriteLine(countABC);
-            }
+                maxLength = Math.Max(maxLength, currentMax); // Обновление максимальной длины
 
+            }
             str = Console.ReadLine();
 
         }
-
-
-        Console.WriteLine("Кол-ыо строк с a*b :" + countAnB);
+        Console.WriteLine("кол-во строк с A*b: " + countAnB);
+        Console.WriteLine("самая длинная подоследовательность abc: " + maxLength);
 
     }
 }
+
